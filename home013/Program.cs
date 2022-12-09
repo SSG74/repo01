dc8e0761e5262c3e9836fb6d5a32c40f14dd473c
Svutlana <fire_fly_74@mail.ru>
2022-12-09 21:04:42 +0500

    home013

---
+++ home013/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите номер дня недели");
+int day = int.Parse(Console.ReadLine());
+
+if (day > 5)
+    Console.WriteLine("да, выходной");
+else
+    Console.WriteLine("нет, рабочий день");
