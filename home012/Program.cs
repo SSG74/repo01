a79234686dd0f265f73716b8bbad565d36a81221
Svutlana <fire_fly_74@mail.ru>
2022-12-09 21:41:15 +0500

    home012

---
+++ home012/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else{
+{
+    while (number > 1000)
+    { number = number / 10;}
+}
+int number1 = number % 10;
+Console. WriteLine($" третья цифра: {number1} ");}    
